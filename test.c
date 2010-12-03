22f77618133b7fbb954e12c3311a2a178a32f43e
pxy <xiaoyue.pan@it.uu.se>
2010-12-03 11:16:57 +0100

    adding the state aware random number generator

--- test.c
+++ test.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <assert.h>
 #define NUM_THREADS 4
+#define PRNG_BUFSZ 32
 #define E 2.71828
 #define SECOND_TO_USECOND 1000000
 #define ARRIVAL_LAMDA 20 //arrival rate = 1/average inter arrival time
@@ -17,74 +18,71 @@
 
 #define LINUX // activate linux specific code
 
-//declare a global mutex
-//an array to store lock contention time
 double time_in_cs[NUM_THREADS];
 double ex_time[NUM_THREADS]; 
 double experiment_time;
 pthread_mutex_t sum_lock;
-int count_contention;
 
 typedef struct
 {
 	int thread_id;
-//	double arrival_time;
+	struct random_data* rand_states;
+	int arrival_lambda;
 }thread_params;
 
-double exp_rand(float miu)
+//Given a randomly generated rand, convert a uniform distribution into an exponenetial distribution
+double exp_rand(float miu,int rand)
 {
-	//srand(time(NULL)); //generate random numbers
-	double ran = (-log((double)rand()/(double)RAND_MAX)/miu);
-	//printf("In exp_ran: %lf\n",ran);
+	double ran = (-log((double)rand/(double)RAND_MAX)/miu);
 	return ran;
 }
-double uniform()
-{
-	return (double)rand()/(double)RAND_MAX;
-}
+
 void *work(void *thread_arg)
 {
-	srand(time(NULL));
+	int i;
 	thread_params *thread_param = (thread_params *)thread_arg;
-	double service_time;
-	//double next_arrival_time;
-	double arrival_time;
+	double service_time, arrival_time;
+	//get the parameters from thread_param
 	int tid = thread_param->thread_id;
-	struct timeval tim;// = cs_time[tid];
-	double t1,t2;
-	struct timeval ex_tim;// = cs_time[tid];
-	double t3,t4; //for measuring the execution time of a thread
-	int i; 
-	//int arrival_time = (int)thread_param->arrival_time;
+	int  arrival_lambda = thread_param->arrival_lambda;
+	struct random_data* arg = thread_param->rand_states;
+	
+	struct timeval tim,ex_tim;
+	double t1,t2,t3,t4;
+
 	gettimeofday(&ex_tim, NULL);
 	t3 = ex_tim.tv_sec*SECOND_TO_USECOND + ex_tim.tv_usec;
 	for(i = 0; i < NUM_LOOP; i++)
 	{
-		//if(i == 0)
-		arrival_time = exp_rand(ARRIVAL_LAMDA);
-		//else
-			//arrival_time = next_arrival_time;
+		int r;
+		//generate a random number according to the arrival lambda
+ 		random_r(arg, &r);
+		arrival_time = exp_rand(arrival_lambda,r);
 #ifdef DEBUG
 		printf("Thread %d arrival time: %lf\n",tid,arrival_time);
 #endif
+		//using usleep to change the arrival time
 		usleep((int)arrival_time*SECOND_TO_USECOND);
 #ifdef DEBUG
 		printf("Thread %d trying to get the lock\n",tid);
 #endif
+
 		gettimeofday(&tim, NULL);
 		t1 = tim.tv_sec*SECOND_TO_USECOND + tim.tv_usec;
-		//next_arrival_time = exp_rand(ARRIVAL_LAMDA); //generate the next arrival time before being served.
+		//start of the critical section
 		pthread_mutex_lock (&sum_lock);
 #ifdef DEBUG
 		printf("Thread %d got the lock\n",tid);
 #endif
-		service_time = exp_rand(SERVICE_MIU);
-		//service_time = 0.125;
+ 		random_r(arg, &r);
+		//printf("random number r in service time is : %d.\n",r);
+		service_time = exp_rand(SERVICE_MIU,r);
 #ifdef DEBUG
 		printf("Service time of Thread %d: %lf.\n",tid,service_time);
 #endif
 		usleep(service_time*SECOND_TO_USECOND);
 		pthread_mutex_unlock (&sum_lock);
+		//End of the critical section
 		gettimeofday(&tim, NULL);
 
 		t2 = tim.tv_sec*SECOND_TO_USECOND + tim.tv_usec;
@@ -95,22 +93,16 @@ void *work(void *thread_arg)
 #endif
 		time_in_cs[tid] += t2 - t1; //- service_time*SECOND_TO_USECOND; 
 		assert(t2-t1 > service_time*SECOND_TO_USECOND);
-		//At the end of the service, check if the time spend in the critical section is already longer than the generated arrival time
-		//if(t2 - t1 > next_arrival_time*SECOND_TO_USECOND)
-			//next_arrival_time = 0;
 	}
-	gettimeofday(&ex_tim, NULL);
-	t4 = ex_tim.tv_sec*SECOND_TO_USECOND + ex_tim.tv_usec;
-	ex_time[tid] = t4 - t3;
-	pthread_exit(NULL);
+		pthread_exit(NULL);
 }
+
 int main (int argc, char *argv[])
 {
 	pthread_t threads[NUM_THREADS];
 	int rc;
 	int t;
 	double sum;
-	count_contention = 0;
 	thread_params para[NUM_THREADS];
 	//double inter_arrival_time[NUM_THREADS];
 	srand(time(NULL));
@@ -120,34 +112,36 @@ int main (int argc, char *argv[])
 	cpu_set_t cpuset[NUM_THREADS]; //for setting affinity of the threads
 	#endif
 	int s;
+	int arrival_lambda;
+	//Initialize random states variable
+	struct random_data* rand_states;
+	char* rand_statebufs;
+	pthread_t* thread_ids;
+	/*allocate memory*/
+	rand_states = (struct random_data*)calloc(NUM_THREADS, sizeof(struct random_data));
+	rand_statebufs = (char*)calloc(NUM_THREADS, PRNG_BUFSZ);
+	thread_ids = (pthread_t*)calloc(NUM_THREADS, sizeof(pthread_t));
+	if(argc < 2)
+	{
+		printf("Usage: ./test arrival_lambda\n");
+		exit(-1);
+	}
+	arrival_lambda = atoi(argv[1]);
+
 	for(i = 0; i < NUM_EXPERIMENTS; i++)
 	{
 		sum = 0;
 		struct timeval tim; double t1,t2; //for timing one experiment
 		gettimeofday(&tim, NULL);
 		t1 = tim.tv_sec*SECOND_TO_USECOND + tim.tv_usec;
-		/*
-		   for(t=0; t<NUM_THREADS; t++)
-		   {
-		//inter_arrival_time[t] = exp_rand(ARRIVAL_LAMDA);
-		inter_arrival_time[t] = uniform();
-		}
-		 */
-		/*
-#ifdef DEBUG
-for(t=0; t<NUM_THREADS; t++)
-printf("printing inter arrival time %d: %lf\n",t,inter_arrival_time[t]);
-#endif
-		 */
-		//para[0].arrival_time = inter_arrival_time[0];
-		/*for(t=1; t<NUM_THREADS; t++)
-		  {
-		  para[t].arrival_time = para[t-1].arrival_time + inter_arrival_time[t];
-		  }*/
 		pthread_mutex_init(&sum_lock, NULL);
 		for(t = 0; t < NUM_THREADS; t++){
 			para[t].thread_id = t;
 			time_in_cs[t] = 0;
+			//for the random variable
+			initstate_r(random(), &rand_statebufs[t], PRNG_BUFSZ, &rand_states[t]);
+			para[t].rand_states = &rand_states[t];
+			para[t].arrival_lambda = arrival_lambda;
 			rc = pthread_create(&threads[t], NULL, work, (void *)&para[t]);
 			//printf("from main, create threads,test test\n");
 			if (rc){
@@ -174,8 +168,8 @@ printf("printing inter arrival time %d: %lf\n",t,inter_arrival_time[t]);
 		pthread_mutex_destroy(&sum_lock);
 		gettimeofday(&tim, NULL);
 		t2 = tim.tv_sec*SECOND_TO_USECOND + tim.tv_usec;
-		printf("The experiment took %.6lf microseconds.\n", t2-t1);
 #ifdef DEBUG
+		printf("The experiment took %.6lf microseconds.\n", t2-t1);
 		for(t=0; t<NUM_THREADS; t++){
 			printf("Time in cs: for thread %d: %lf \n",t,time_in_cs[t]);
 			printf("Execution time for thread %d: %lf \n",t,ex_time[t]); 
@@ -184,7 +178,7 @@ printf("printing inter arrival time %d: %lf\n",t,inter_arrival_time[t]);
 
 		for(t = 0; t < NUM_THREADS; t++)
 			sum += time_in_cs[t];
-		printf("**********************************Average waiting time: %lf\n",sum/(double)(NUM_THREADS*NUM_LOOP));
+		printf("Average response time for lambda = %d: %lf\n",arrival_lambda,sum/(double)(NUM_THREADS*NUM_LOOP));
 		//contention_time  += sum/(double)NUM_THREADS;
 		//printf("Contention percentage: %lf\n", (sum/(double)NUM_THREADS)/(t2-t1));
 	}
