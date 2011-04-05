fa7969e5d1fa4cf73c27f98857a81f88e97945bf
jonatan <jonatan.linden@it.uu.se>
2011-04-05 14:23:51 +0200

    updated gspn tools and added parametrized model

--- distr_of_interarrival.hs
+++ distr_of_interarrival.hs
@@ -17,15 +17,27 @@ waittime :: (Num a) => [a] -> a
 waittime [] = 0
 waittime (x1:x2:xs) = x2 - x1 + waittime xs
 
+main3 = do dat <- getContents
+           parsed <- myparse dat
+           putStrLn $ show $ (waittime (map fst (sortBy (comparing snd) parsed)) / ((fromIntegral (length parsed)) / 2))
+           return ()
+
+
 main = do dat <- getContents
-          parsed <- myparse dat
-          putStrLn $ show $ (waittime (map fst (sortBy (comparing snd) parsed)) / ((fromIntegral (length parsed)) / 2))
+          parsed <- myparse dat -- already sorted
+          
           return ()
+
 
  -- parse it
 myparse input = case parse file "" input of
 	          Left er  -> return []
 	          Right cl -> return $ cl
+
+myparse2 input = case parse file2 "" input of
+	          Left er  -> return []
+	          Right cl -> return $ cl
+
 	 
 -- | function generating a token parser based on a
 -- lexical parsers combined with a language record definition
@@ -43,10 +55,21 @@ row = do (char 'r' <|> char 't') >> space
          x1 <- double
          x2 <- integer
          return (x1, x2)
+
+row2 :: CharParser st (Integer, Integer)
+row2 = do (char 'r' <|> char 't') >> space
+          x1 <- integer
+          x2 <- integer
+          return (x1, x2)
+
          
 file :: CharParser st [(Double, Integer)]
 file = many row
 
+
+-- this one parses a file of two ints on each row
+file2 :: CharParser st [(Integer, Integer)]
+file2 = many row2
 
 {-
 6: 1660
--- gspn_tools/spn_script.sh
+++ gspn_tools/spn_script.sh
@@ -5,5 +5,5 @@ for i in {100000..1000000..100000}; do
     spn_update.py --name lambda --val $i tmp2 > "$1.net"
     spn_solve.sh "$1" &> /dev/null
     echo -n "$i "
-    spn_calc.py -t T9 -p P2 P4 P5 P6 -- "$1"
+    echo "(4*`spn_calc.py -t T9 -p P2 P6 -- "$1"` + 2*`spn_calc.py -t T8 -p P9 P13 -- "$1"`)/6" | sed 's/e/\*10\^/g' | bc -l
 done
