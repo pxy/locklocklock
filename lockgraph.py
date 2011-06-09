3fd021bef92dad23ad19d20c77066fed91ca6ddd
jonatan <jonatan.linden@it.uu.se>
2011-06-09 14:38:35 +0200

    lockgraph working

--- lockgraph.py
+++ lockgraph.py
@@ -68,7 +68,35 @@ def sumMatrices (mtcs):
     return mtxG
 
 
-    
+# functions to add/increment values in dictionaries
+
+def addCond (dict, idx, val):
+	if idx in dict.keys():
+		dict[idx] += val
+	else:
+		dict[idx] = val
+
+def appendCond (dict, idx, elem):
+	if idx in dict.keys():
+		dict[idx].append(elem)
+	else:
+		dict[idx] = [elem]
+
+def extendCond (dict, idx, l):
+	if idx in dict.keys():
+		dict[idx].extend(l)
+	else:
+		dict[idx] = l
+
+def revertListDict (dict):
+    '''dict : a dictionary with lists as values
+    PRE : each value in each of the lists are unique
+    '''
+    retD = {}
+    for k,v in dict.iteritems():
+        for i in v:
+            retD[i] = k
+    return retD
 
 # end UTILS
 # --------------------------------------------------------------------------
@@ -97,10 +125,6 @@ def filterByLockName (lockNames, filterName):
     res = map(lambda x: bool(regex.search(x)), lockNames)
     print res
     return
-
-
-    
-
 
 def prune (mtx, filter):
     '''Prunes a matrix in all dimensions, based on the boolean vector filter
@@ -137,20 +161,17 @@ def waitingTimeParse (file):
 	
 
 def lockDictFromRecords(recFile):
-    '''Parses a record on the format "tsc tID lID" and returns a
-    dictionary where the key is the thread id, and the value is an
-    ordered list of (lID, tsc) tuples. Assumes that the list is sorted
-    in increasing order by timestamp
-    '''
-    tidDict = {}
-    recReader = csv.reader (recFile, delimiter=' ', skipinitialspace=True)
-    for _row in recReader:
-        row = map (int, filter(None, _row))
-        if row[1] in tidDict.keys():
-            tidDict[row[1]].append((row[2], row[0]))
-        else:
-            tidDict[row[1]] = [(row[2], row[0])]
-    return tidDict
+	'''Parses a record on the format "tsc tID lID" and returns a
+	dictionary where the key is the thread id, and the value is an
+	ordered list of (lID, tsc) tuples. Assumes that the list is sorted
+	in increasing order by timestamp
+	'''
+	tidDict = {}
+	recReader = csv.reader (recFile, delimiter=' ', skipinitialspace=True)
+	for _row in recReader:
+		row = map (int, filter(None, _row))
+		appendCond (tidDict, row[1], (row[2], row[0]))
+	return tidDict
 
 def parseInstrList (instrFile):
     '''Input file should be on the format "lID randomcrap [hexaddr]"
@@ -163,50 +184,48 @@ def parseInstrList (instrFile):
     return res
 
 
-
 def parseDicTuple (prefix, ths):
-    acqfile = open(prefix + "_" + str(ths) + "th_acq.dat")
-    acqDic = lockDictFromRecords(acqfile)
+	'''Parses three related files, based on a file path prefix and the number
+	of threads the files represent.
+	Assumes the data files follow the naming convention appname_{#threads}th_{acq,rel}.dat
+	prefix should be the path, including the appname.
+	'''
+	acqfile = open(prefix + "_" + str(ths) + "th_acq.dat")
+	acqDic = lockDictFromRecords(acqfile)
 
-    relfile = open(prefix + "_" + str(ths) + "th_rel.dat")
-    relDic = lockDictFromRecords(relfile)
+	relfile = open(prefix + "_" + str(ths) + "th_rel.dat")
+	relDic = lockDictFromRecords(relfile)
 
-    creationFile = open(prefix + "_lid_instr_" + str(ths) + "th.dat")
-    createVec = creationParse(creationFile)
+	creationFile = open(prefix + "_lid_instr_" + str(ths) + "th.dat")
+	createVec = creationParse(creationFile)
 
-    creationFile.seek(0)
-    instrVec = parseInstrList(creationFile)
+	creationFile.seek(0)
+	instrVec = parseInstrList(creationFile)
 
-    acqfile.close()
-    relfile.close()
-    creationFile.close()
-    
-    return (acqDic, relDic, createVec, instrVec)
+	acqfile.close()
+	relfile.close()
+	creationFile.close()
 
+	return (acqDic, relDic, createVec, instrVec)
 
 
 # end PARSING
 # --------------------------------------------------------------------------
 
 
-
 # input is dictionary with tID as key, sequence of lock accesses as value
 def countMtxFromSeq(lockSeq):
-    '''Computes a nested dictionary with lock transition counts,
+	'''Computes a nested dictionary with lock transition counts,
 	or equivalently, a transition matrix with counts.
-    '''
+	'''
 	# use dictionary instead of matrix, since we don't know the total
 	# number of locks involved
-    lockD = collections.defaultdict(dict)
-    last = lockSeq[0][0]
-    for lID in lockSeq[1:]:
-        if lID[0] in lockD[last]:
-            lockD[last][lID[0]] += 1 
-        else:
-            lockD[last][lID[0]] = 1
-        last = lID[0]
-    return dictToMatrix(lockD)
-
+	lockD = collections.defaultdict(dict)
+	last = lockSeq[0][0]
+	for lID in lockSeq[1:]:
+		addCond (lockD[last], lID[0], 1)
+		last = lID[0]
+	return dictToMatrix(lockD)
 
 # FIX NOW !
 def timedTransitions (lockSeq, relLockSeq):
@@ -217,34 +236,73 @@ def timedTransitions (lockSeq, relLockSeq):
     # subtract relLock_0 from trylock_1, as in (lockID_0, trylock_1)
     timeD = collections.defaultdict(dict)
     for i, lID in enumerate(lockSeq[1:]):
-        if lID[0] in timeD[relLockSeq[i][0]]:
-            timeD[relLockSeq[i][0]][lID[0]] += lID[1] - relLockSeq[i][1]
-        else:
-            timeD[relLockSeq[i][0]][lID[0]] =  lID[1] - relLockSeq[i][1]
+		addCond (timeD[relLockSeq[i][0]], lID[0], lID[1] - relLockSeq[i][1])
     return timeD
 
 
 def waitingTime (acqLockSeq, relLockSeq):
-    """Calculates average waiting time (service time + queue time) per lock
-    acqLockSeq, relLockSeq : a tuple list of the form (lockID, timestamp)
-    """
+	'''POST: Dictionary containing all individual waiting times for each lock
+	'''
     # waiting time is:
     # (lockID_0, trylock_0), find following (lockID_0, relLock_0) in rel
     # subtract tryLock_0 from relLock_0
-    timeD = collections.defaultdict(dict)
-    for i, acq in enumerate(acqLockSeq):
-        rel = relLockSeq[i]
-        if acq[0] != rel[0]:
-            print "ERROR: lock sequences not synced"
-        if acq[0] in timeD.keys():
-            timeD[rel[0]].append(rel[1] - acq[1])
-        else:
-            timeD[rel[0]] = [rel[1] - acq[1]]
-    size = max (timeD.keys()) + 1
-    arr = np.zeros(size)
-    for k,v in timeD.iteritems():
-        arr[k] = float(sum (v))/len(v)
-    return arr
+	timeD = collections.defaultdict(dict)
+	for i, acq in enumerate(acqLockSeq):
+		rel = relLockSeq[i]
+		if acq[0] != rel[0]:
+			print "ERROR: lock sequences not synced"
+		appendCond(timeD, rel[0], rel[1] - acq[1])
+	return timeD
+
+def sumWaitingTime (acqSeq, relSeq):
+	sumWait = 0
+	for i, acq in enumerate(acqSeq):
+		rel = relSeq[i]
+		if acq[0] != rel[0]:
+			print "ERROR: lock sequences not synced"
+		sumWait += rel[1] - acq[1]
+	return sumWait
+    
+def avgWaitTime (acqLockSeq, relLockSeq):
+	"""Calculates average waiting time (service time + queue time) per lock
+    INPUT: acqLockSeq, relLockSeq : a tuple list of the form (lockID, timestamp)
+    """
+	timeD = waitingTime (acqLockSeq, relLockSeq)
+	size = max (timeD.keys()) + 1
+	arr = np.zeros(size)
+	countArr = np.zeros(size)
+	for k,v in timeD.iteritems():
+		# assumes lock ids appear in order without gaps
+		countArr[k] = len(v)
+		lo = len(v)/3
+		hi = 2*len(v)/3
+		if hi == lo:
+			hi = len (v)
+			lo = 0
+		arr[k] = float(sum(v[lo:hi])) / (hi - lo)
+	return (arr, countArr)
+
+def servTime (acqD, relD):
+	servTimeList = map (avgWaitTime, acqD.values(), relD.values())
+
+	servTimes, counts = zip (*servTimeList)
+
+	count = np.zeros_like(max(counts, key=len))
+
+	for c in counts:
+		print str(len(c))
+		count[0:len(c)] += c
+
+	norms = map (lambda x: x / count[:len(x)] * 1.0, counts)
+
+	# weighted average
+
+	servTimeArr = np.zeros_like( max (servTimes, key = len))
+	for i,l in enumerate(servTimes):
+		l[0:len(norms[i])] *= norms[i]
+		l[len(norms[i]):] = np.zeros(l.shape[0] - len(norms[i]))
+		servTimeArr[0:len(l)] += l
+	return (servTimeArr, count)
 
 
 def sumTimeMtx (acqSeq, relSeq):
@@ -262,80 +320,71 @@ def avgTimeMtx (acqSeq, relSeq, countM):
     r = np.maximum(countM, np.ones((size,size)))
     return np.divide(sumTimeM, r)
 
+
 def hexToLine (createVec_, appname):
-    # convert each hex address to line in code for both lists
-    createVec = []
+	'''converts each hex address in createVec to a filename and row number
+	'''
+	process = subprocess.Popen([ADDR2LINEPATH, '-e', appname], shell=False,
+							   stdout=subprocess.PIPE,
+							   stdin=subprocess.PIPE
+							   )
+	createVec = []
+	for addr in createVec_:
+		process.stdin.write(addr + '\n') # write address to addr2line, fetch line
+		createVec.append(process.stdout.readline().rpartition('/')[2].rstrip())
 
-    process = subprocess.Popen([ADDR2LINEPATH, '-e', appname], shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    for addr in createVec_:
-        process.stdin.write(addr + '\n')
-        createVec.append(process.stdout.readline().rpartition('/')[2].rstrip())
-
-    process.terminate()
-    
-    return createVec
+	process.terminate()
+	return createVec
 
 
-def revertListDict (dict):
-    '''dict : a dictionary with lists as values
-    PRE : each value in each of the lists are unique
-    '''
-    retD = {}
-    for k,v in dict.iteritems():
-        for i in v:
-            retD[i] = k
-    return retD
+def lockToServTimeMap (instrVec, servTime, countVec):
+	'''POST: a map from instructions to weighted averaged service times
+	'''
+	lockD1 = {}
+	for i,k in enumerate(instrVec):
+		appendCond (lockD1, k, i)
 
-def lockToServTimeMap (instrVec, servTime):
-    lockD1 = {}
-    for i,k in enumerate(instrVec):
-        if k in lockD1.keys():
-            lockD1[k].append(i)
-        else:
-            lockD1[k] = [i]
+	# servD1 instr -> service time
+	servD1 = {}
+	for k in lockD1.keys():
+		filtered = []
+		keys = []
+		for x in lockD1[k]:
+			if x < len(servTime):
+				keys.append(x)
+				filtered.append(servTime[x])
+		norm_ = []
+		for key in keys:
+			norm_.append(countVec[key])
+		sumn = sum(norm_)
+		norm = np.array(norm_)
+		norm /= sumn * 1.0
+		servD1[k] = sum(np.array(filtered)*norm)
+	return servD1
 
-    # lockD1 instr -> lockID list
 
-    # servD1 instr -> service time
-    servD1 = {}
-    for k in lockD1.keys():
-        print k, lockD1[k]
-        filtered = []
-        for x in lockD1[k]:
-            if x < len(servTime):
-                filtered.append(servTime[x])
-        servD1[k] = float(sum(filtered)) / len(filtered)
-    return servD1
-
-def lockMap (createVec1_, createVec2_, servTimeVec2):
+def lockMap (createVec1_, createVec2_, servTimeVec2, countVec2):
     '''Creates a surjective map from vector 1 to vector 2
     createVec1_ and createVec2_ should be lists of some text uniquely
     identifying a piece of code
     '''
     # create dict indexed by string (filename + linenumber) from 1 th case
-
-    servD1 = lockToServTimeMap (createVec2_, servTimeVec2)
+    servD1 = lockToServTimeMap (createVec2_, servTimeVec2, countVec2)
 
     lockD = {}            
     for i,k in enumerate(createVec1_):
-        if k in lockD.keys():
-            lockD[k].append(i)
-        else:
-            lockD[k] = [i]
-
-    print servD1
+		appendCond (lockD, k, i)
 
     # lockD instr -> lockID list
     mapp = revertListDict (lockD)
 
-    # create surjective mapping from values (8 th case) to keys (1 th case)
-
     # TODO: if one lock (row of code) does not appear in lockD1, what to do?
+	# FIX: correct missing indices manually
     #retD = {}
     #for k in mapp.keys():
     #    retD[k] = servD1[mapp[k]]
+	#servTimeMapp = dictToArray (retD)
     return servD1, mapp
-    #fix missing indices manually
 
 
 def insertIntermediateQs (rMatrix, tMatrix, tArray):
@@ -371,6 +420,12 @@ def pruneAll (rMtx, tMtx, tVec, epsilon):
     
     return prune(rMtx, f), prune(tMtx, f), prune(tVec, f), ids
 
+
+
+#--------------------------------------------------------------------------
+# entry point of application
+
+
 def analyze (acqDic, relDic, servTimeVec_, numT, lockNames):
     print 'calculating matrices'
     cntMtcs = map (countMtxFromSeq, acqDic.values())
@@ -384,24 +439,17 @@ def analyze (acqDic, relDic, servTimeVec_, numT, lockNames):
     if sumInterArrivalTotalM.shape[0] != cntTotalM.shape[0]:
         print "WARNING: count matrix not same size as interarrival time matrix."
 
-
-    
     # After this point, the mapping between locks in the service time array and the
     # matrices should be one to one
 
     servTimeVec = servTimeVec_[0:cntTotalM.shape[0]]
-        
     
-    #if cntTotalM.shape[0] != servTimeVec.shape[0]:
-    #    print "ERROR: service time vector not the same size as count matrices. Correcting (not good solution)."
-    
-
     # calculate avg transition time
     r = np.maximum(cntTotalM, np.ones_like (cntTotalM))
     avgInterArrivalTotalM = np.divide (sumInterArrivalTotalM, r)
 
     # prune locks not used very much
-    cntP, avgInterArrivalP, tVecP, idMap = pruneAll (cntTotalM, avgInterArrivalTotalM, servTimeVec, 1000)
+    cntP, avgInterArrivalP, tVecP, idMap = pruneAll (cntTotalM, avgInterArrivalTotalM, servTimeVec, 100)
 
     # normalize (row-wise) the pruned transition count matrix to get the routing matrix
     routP = normalizeRowWise (cntP)
@@ -428,52 +476,46 @@ def analyze (acqDic, relDic, servTimeVec_, numT, lockNames):
     print [(i, row) for i, row in enumerate (idMap)]
     return 
 
+#--------------------------------------------------------------------------
+
+
 def main():
-    global options
-    parser = ArgumentParser()
-    parser.add_argument("-v", dest="verbose", action="store_true", default=False,
-                        help="print more information.")
-    parser.add_argument("-d", "--debug",
+	global options
+	parser = ArgumentParser()
+	parser.add_argument("-v", dest="verbose", action="store_true", default=False,
+						help="print more information.")
+	parser.add_argument("-d", "--debug",
 						action="store_true", dest="debug", default=False,
 						help="print debug information")
-    parser.add_argument("-n", type=int, dest="numCores", nargs='?', help="Number of customers in queueing network.")
-    parser.add_argument("datafile", help="Prefix of files containing thread IDs, lock IDs and acquire timestamps, sorted by time. Name should be \"prefix_{#threads}th_{acq,rel}.dat\"")
-    parser.add_argument("servfile", help="File containing lock IDs and average service time, sorted by lock ID.")
+	parser.add_argument("-n", type=int, dest="numCores", nargs='?', help="Number of customers in queueing network.")
+	parser.add_argument("datafile", help="Prefix of files containing thread IDs, lock IDs and acquire timestamps, sorted by time. Name should be \"prefix_{#threads}th_{acq,rel}.dat\"")
+	parser.add_argument("servfile", help="File containing lock IDs and average service time, sorted by lock ID.")
 
+	options = parser.parse_args()
 
-    options = parser.parse_args()
+	if options.debug:
+		print >> sys.stderr, "options:", options
 
-    if options.debug:
-        print >> sys.stderr, "options:", options
+	# parse data for 8 thread case
+	(acq8D, rel8D, create8Vec, instr8Vec_) = parseDicTuple (options.lockacq, 8)
 
-    # we assume the data files follow the naming convention appname_{#threads}th_{acq,rel}.dat
-    
-    servfile = open(options.servfile)
-    servTimeVec = waitingTimeParse(servfile)
-    servfile.close()
+	# parse data for 1 thread case
+	(acq1D, rel1D, create1Vec, instr1Vec_) = parseDicTuple (options.lockacq, 1)
 
-    # parse data for 8 thread case
-    (acqDic, relDic, create8Vec) = parseDicTuple (options.lockacq, 8)
+	(servTimeArr, count) = servTime (acq1D, rel1D)
 
-    # parse data for 1 thread case
-    (acq1Dic, rel1Dic, create1Vec) = parseDicTuple (options.lockacq, 1)
+	(servD1, mapp) = lockMap (hexToLine(instr8Vec_, '/Users/jonatanlinden/res/dedup'), hexToLine(instr1Vec_, '/Users/jonatanlinden/res/dedup'), servTimeArr, count)
 
-    servTimeList = map (waitingTime, acq1D.values(), rel1D.values())
+	# add missing entries to servD1 manually, then run the following code
+	#mapD
+	#for k in mapp.keys():
+	#    mapD[k] = servD1[mapp[k]]
+	#servTimeVec = dictToArray (mapD)
+	
+	analyze (acqDic, relDic, servTimeVec, options.numCores)
 
-    servTimeArr = np.zeros_like( max (servTimeList, key = len))
-    for l in servTimeList:
-        servTimeArr[0:len(l)] += l
-    servTimeArr /= len (servTimeList)
-
-
-    lockMap (create8Vec, create1Vec)
-    
-
-    analyze (acqDic, relDic, servTimeVec, options.numCores)
-
-    sys.exit(0)
+	sys.exit(0)
     # END MAIN
-
 
 # uncomment when running from command line
 
