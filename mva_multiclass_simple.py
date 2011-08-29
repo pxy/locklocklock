d7937de8f19d577c6c53aed9818e7eaec1791ad8
jonatan <jonatan.linden@it.uu.se>
2011-08-29 18:59:50 +0200

    major indexing overhaul, mostly to facilitate returning values specific to one pop.vector in mva_multiclass

--- mva_multiclass_simple.py
+++ mva_multiclass_simple.py
@@ -6,21 +6,6 @@ from numpy.linalg import solve
 from collections import defaultdict
 np.set_printoptions(threshold=np.nan)
 
-#TODO: given the number of classes and maximum number of jobs in each class, return the list of all possible population vectors
-def getV(l):
-    zero = tuple(np.zeros(len(l), dtype=int))
-
-    if [zero] == l:
-        return l
-    else:
-        s = l
-        for i in l:
-            s = s + getV(dependentsV(i))
-        return list(set(s))
-
-
-
-#	return [(1,0),(0,1),(1,1),(2,0),(0,2),(2,1),(1,2),(2,2)]
 
 def getPopulationVs (nClassL, exists):
     if exists[nClassL]:
@@ -33,20 +18,9 @@ def getPopulationVs (nClassL, exists):
 def unitVs (dim):
     return map (tuple, np.identity(dim, dtype=int))
 
-# def getDependentV((a,b)):
-# 	if a > 0 and b > 0:
-# 		return [(a-1,b),(a,b-1)]
-# 	elif a > 0:
-# 		return [(a-1,b)]
-# 	elif b > 0:
-# 		return [(a,b-1)]
-# 	else:
-# 		return []
-
 def dependentsV(tup):
     units = unitVs (len(tup))
-    return filter (lambda x: all(i >= 0 for i in x), map (lambda x: tuple(np.subtract(tup,x)), units))
-
+    return filter (lambda x: all(i >= 0 for i in x), [tuple(np.subtract(tup,x)) for x in units])
 
 
 #This function implements the mean value analysis for the multi-class closed network
@@ -57,74 +31,63 @@ def mva_multiclass(routL, servrates, nClassL, queueType):
     K = len(servrates) #total number of queues
     n_class = len(routL) #the number of classes
     n_threads = sum (nClassL)
-    all_popuV = list (getPopulationVs(tuple(nClassL), defaultdict(int))) #getV([tuple(nClassL)]) #the finial population vector
-    #all_popuV = getV([tuple(nClassL)]) #the finial population vector
-    e = getVisitRatios(routL)
+    all_popuV = list (getPopulationVs(tuple(nClassL), defaultdict(int))) #the final population vector
+
+    e = np.array(getVisitRatios(routL))
     #print "The visit ratio:", e
-    #print "The total number of queues:", K
-    #print "The total number of threads:", n_threads
-    #print "The total number of classes:", n_class
-    print "The final population vector:", all_popuV
-    #step 1: initialize the number of jobs matrix, it is a matrix of dictionary with i: the node index and r: the class index as the indices and the key of the dictionary is a population matrix
-    T = np.empty([K,n_class],dtype=dict)
-    N = np.empty([K,n_class],dtype=dict) #N is K in the algorithm
-    lam = np.empty([n_class],dtype=dict)
-    for i in range(0,K):
-        for r in range(0,n_class):
-            T[i][r] = {}
-            N[i][r] = {}
-            for k in all_popuV:
-                T[i][r][k] = 0.0
-                N[i][r][k] = 0.0
 
-    for r in range(0,n_class):
-        lam[r] = {}
-        for k in all_popuV:
-            lam[r][k] = 0.0
-
-
-    #step 2.1
+    #STEP 1: initialize the number of jobs matrix, it is a matrix of dictionary with i:
+    # the node index and r: the class index as the indices and the key of the dictionary is a population matrix
+    T = {}
+    N = {}
+    lam = {}
     for k in all_popuV:
-    #all_popuV.remove((0,0))
-    #for k in all_popuV:
-	#print "step 2.1 for population vector ", k
+        T[k] = np.zeros((K,n_class))
+        N[k] = np.zeros((K,n_class))
+        lam[k] = np.zeros(n_class)
+
+    #STEP 2.1
+    for k in all_popuV:
+    #print "step 2.1 for population vector ", k
         for i in range(0,K):
             for r in range(0,n_class):
                 if queueType[i] == 1:#if node i is an infinite server node, the response time is just the service time	
-                    T[i][r][k] =  1.0/servrates[i][r]
+                    T[k][i][r] =  1.0/servrates[i][r]
                 else: #if node i is a single server queue
                     sum_less = 0.0
-                    for s in range(0,n_class): #from 0 to the number of classes
-                        l = dependentsV(k)
-                        for z in l:
-                            sum_less += N[i][s][z]
-                            
-                    T[i][r][k] = (1.0/servrates[i][r])*(1.0+sum_less)
+                    l = dependentsV(k)
+                    for z in l:
+                        # for each class, sum together the total number of customers waiting
+                        sum_less += sum(N[z][i])
+
+                    T[k][i][r] = (1.0/servrates[i][r])*(1.0+sum_less)
             #print "T[",i+1,"][",r+1,"][",k, "] = ", T[i][r][k], "1/u: ", 1.0/servrates[i][r], " sum_less: ", sum_less
-    #step 2.2
+        #STEP 2.2
         for r in range(0,n_class):
-            sum2 = 0.0
-            for i in range(0,K):
-                sum2 += e[r][i]*T[i][r][k]
+            #for each class/row, sum together expected time
+            sum2 = np.dot(e[r],T[k][:,r])
 			#print "e[",i+1,"]",e[i],"T[",i+1,"]","[",r+1,"]","[",k,"]",T[i][r][k],"print sum2 in 2.2:", sum2
-            lam[r][k] = k[r]/sum2
+            lam[k][r] = k[r]/sum2
 		#print "lam",r+1,k, "=", lam[r][k]
-		#print the throughput of each class for each queue
-        if k == tuple(nClassL):
-		#for i in range(0,K): it's enough to print the info for one queue
-            lam1 = e[0][2]*lam[0][k]
-            lam2 = e[1][2]*lam[1][k]
-		#print "Throughput for queue ", i, " class 1: ", lam1, "class 2:",lam2, "throughput difference: ", lam1 - lam2
-		#print lam1, lam2, "throughput difference: ", lam1 - lam2
-    #step 2.3
+
+        #STEP 2.3
+        #for each class and each server, update est. no. of customers.
+        # Question: why do we only use half of the values in e?
         for i in range(0,K):
-            for r in range(0,n_class):
-                N[i][r][k] = lam[r][k]*T[i][r][k]*e[r][i]
-        if k == tuple(nClassL):
-            for i in range(0,K):
-                for r in range(0,n_class):
-                    print "N",i+1,r+1,k, "=", lam[r][k],"*",T[i][r][k],"*",e[r][i], "=",N[i][r][k]
-            return  [lam1,lam2]
+            N[k][i] = lam[k]*T[k][i]*e[0:2,i]
+
+    # END for over pop.vectors.
+
+    lam1 = e[0][2]*lam[k][0]
+    lam2 = e[1][2]*lam[k][1]
+    #print "Throughput for queue ", i, " class 1: ", lam1, "class 2:",lam2, "throughput difference: ", lam1 - lam2
+
+
+    for i in range(0,K):
+        for r in range(0,n_class):
+            print "N",k,i+1,r+1, "=", lam[k][r],"*",T[k][i][r],"*",e[r][i], "=",N[k][i][r]
+
+    return  [lam1,lam2], T[nClassL]
 
 
 def getVisitRatios(routL):
@@ -157,7 +120,7 @@ def updateRates(ratesL, adjustIndex, newVal):
 def adjustRate(routL, servrates, cusL, qTypeL, adjustIndex, maxError):
 	#print "In adjustRate:"
 	rateToAdjust = servrates[adjustIndex[0]][adjustIndex[1]]
-	lamL = mva_multiclass(routL, servrates, cusL, qTypeL)
+	lamL, T = mva_multiclass(routL, servrates, cusL, qTypeL)
 	v = 0.1
 	diff = lamL[0] - lamL[1]
 	while math.fabs(diff) > maxError:
@@ -171,7 +134,7 @@ def adjustRate(routL, servrates, cusL, qTypeL, adjustIndex, maxError):
 			rateToAdjust -= v#decrease the rate of the reader
 		#plug ratetoAdjust back to the rates
 		servrates = updateRates(servrates, adjustIndex, rateToAdjust)
-		lamL = mva_multiclass(routL, servrates, cusL, qTypeL)
+		lamL, T = mva_multiclass(routL, servrates, cusL, qTypeL)
 		#print "newServrates:"
 		#print servrates
 		v = v/2
@@ -187,12 +150,12 @@ def runmepan(msg):
     for x in range(10,11):
         #print "service rate for the reader local computation: ",x
         servrates = [[1.0,1.0],[1.0,1.0],[1.0,x],[1.0,0.7]]
-        la = mva_multiclass([rout,rout], servrates, [2,2], [1,0,1,0])
+        la,T = mva_multiclass([rout,rout], servrates, (2,2), [1,0,1,0])
         print "lambda before adjusting rates"
         print la[0],la[1]
         print "The adjusted rates for "
         print x
-        newRates = adjustRate([rout,rout], servrates, [2,2], [1,0,1,0], [3,1],la[0]/100.0)
+        newRates = adjustRate([rout,rout], servrates, (2,2), [1,0,1,0], [3,1],la[0]/100.0)
         print newRates
-        l = mva_multiclass([rout,rout], newRates, [2,2], [1,0,1,0])
+        l,T = mva_multiclass([rout,rout], newRates, (2,2), [1,0,1,0])
         print l[0],l[1]
