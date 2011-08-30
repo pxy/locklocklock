d984472e8ad682151846c7bbaa922d12d10288ea
jonatan <jonatan.linden@it.uu.se>
2011-08-30 10:50:16 +0200

    introduced a single class test case and fixed a bug

--- mva.py
+++ mva.py
@@ -4,42 +4,43 @@ import os,sys,operator,math
 import numpy as np
 from numpy.linalg import solve
 
-def mva(p, servrates, M):
+def mva(p, u, M):
     K = p.shape[0] #number of queues
-    u = servrates
-    #q_type = []
-    q_type = map (lambda (i): i % 2, range(K+1))
-    #for i in range(K + 1):
-    #    q_type.append((i) % 2) # queue type, 0: inf, 1: 1-server
 
-    # A K by K identify matrix to be used later to solve the traffic equation
-    I = np.identity(K) 
+    # we assume every second queue is infinite server (i.e., think time)
+    q_type = np.array(map (lambda (i): i % 2, range(K)))
 
-    #substitute the last row in the transpose matrix of I-p with an array with ones
-    # and try to solve that equation (normalize the ratios in teh traffic equation)
-    q = np.ones(K)
-    tmp = (I-p).T
-    r = np.vstack((tmp[:-1,:],q)) 
-    
+    v = getVisitRatio(p)
+
     N = np.zeros((K,M+1)) # matrix for queue length of node K with M customers 
     W = np.zeros((K,M+1)) # response time of node K with M customers
 
-    a = np.zeros(K) # Zero vector with the last element being 1.0
-    a[K-1] = 1.0
-
-    v = solve(r,a)
-
     for m in range(1,M+1): #[1..M] #loop over the number of customers
+        # first column (m) is always zero
         #step 1
-        for k in range(1,K+1): #loop over the queues
-            W[k-1,m] = (q_type[k-1]*N[k-1,m-1] + 1.0)/u[k-1]
-#        test = (q_type*N[:,m-1] + 1.0)
+        # for each queue, update the waiting time
+        W[:,m] = (q_type*N[:,m-1] + 1.0)/u
 		#step2
-        sum = np.sum (W[:,m]*v)
+        sum = np.dot (W[:,m],v)
         lam = m/sum
-        
         #step3
         N[:,m] = lam*v*W[:,m]
 
     # return the waiting times for the case when M customers are in the network
     return W[:,M]
+
+
+def getVisitRatio(p):
+    K = len(p) #number of queues
+    # A K by K identify matrix to be used later to solve the traffic equation
+    I = np.identity(K)
+    #substitute the last row in the transpose matrix of I-p with an array with ones
+    # and try to solve that equation (normalize the ratios in teh traffic equation)
+    q = np.ones(K)
+    tmp = (I-p).T
+    r = np.vstack((tmp[:-1,:],q))
+
+    a = np.zeros(K) # Zero vector with the last element being 1.0
+    a[K-1] = 1.0
+    v = solve(r,a)
+    return v
--- mva_multiclass_simple.py
+++ mva_multiclass_simple.py
@@ -28,19 +28,20 @@ def dependentsV(tup):
 #	 servrates: service rates for the queues for different job classes, should be a #queues x # classes matrix
 #	 nClassL: a list of the number of threads in each class, the length of the list is the number of classes
 def mva_multiclass(routL, servrates, nClassL, queueType):
-    K = len(servrates) #total number of queues
+    K = servrates.shape[0] #total number of queues
     n_class = len(routL) #the number of classes
     n_threads = sum (nClassL)
-    all_popuV = list (getPopulationVs(tuple(nClassL), defaultdict(int))) #the final population vector
+    all_popuV = list (getPopulationVs(nClassL, defaultdict(int))) #the final population vector
 
     e = np.array(getVisitRatios(routL))
-    #print "The visit ratio:", e
+    # Question: what does the rows of e describe and 
+    print "The visit ratio:", e
 
     #STEP 1: initialize the number of jobs matrix, it is a matrix of dictionary with i:
     # the node index and r: the class index as the indices and the key of the dictionary is a population matrix
     T = {}
     N = {}
-    lam = {}
+    lam = {} # the rate of each class for a specific population vector
     for k in all_popuV:
         T[k] = np.zeros((K,n_class))
         N[k] = np.zeros((K,n_class))
@@ -50,31 +51,32 @@ def mva_multiclass(routL, servrates, nClassL, queueType):
     for k in all_popuV:
     #print "step 2.1 for population vector ", k
         for i in range(0,K):
-            for r in range(0,n_class):
-                if queueType[i] == 1:#if node i is an infinite server node, the response time is just the service time	
-                    T[k][i][r] =  1.0/servrates[i][r]
-                else: #if node i is a single server queue
-                    sum_less = 0.0
-                    l = dependentsV(k)
-                    for z in l:
-                        # for each class, sum together the total number of customers waiting
-                        sum_less += sum(N[z][i])
+            #for r in range(0,n_class):
+            if queueType[i] == 1:#if node i is an infinite server node, the response time is just the service time
+                T[k][i] =  1.0/servrates[i]
+            else: #if node i is a single server queue
+                sum_less = 0.0 
+                l = dependentsV(k)
+                for z in l:
+                    # for each class, sum together the total number of customers waiting
+                    sum_less += sum(N[z][i])
 
-                    T[k][i][r] = (1.0/servrates[i][r])*(1.0+sum_less)
+                T[k][i] = (1.0/servrates[i])*(1.0+sum_less) # R_ck
             #print "T[",i+1,"][",r+1,"][",k, "] = ", T[i][r][k], "1/u: ", 1.0/servrates[i][r], " sum_less: ", sum_less
+
         #STEP 2.2
         for r in range(0,n_class):
             #for each class/row, sum together expected time
-            sum2 = np.dot(e[r],T[k][:,r])
+            sum2 = np.dot(e[r, :],T[k][:,r])
 			#print "e[",i+1,"]",e[i],"T[",i+1,"]","[",r+1,"]","[",k,"]",T[i][r][k],"print sum2 in 2.2:", sum2
-            lam[k][r] = k[r]/sum2
+            lam[k][r] = k[r]/sum2 # 
 		#print "lam",r+1,k, "=", lam[r][k]
 
         #STEP 2.3
         #for each class and each server, update est. no. of customers.
         # Question: why do we only use half of the values in e?
         for i in range(0,K):
-            N[k][i] = lam[k]*T[k][i]*e[0:2,i]
+            N[k][i] = lam[k]*T[k][i]*e[:,i]
 
     # END for over pop.vectors.
 
@@ -87,7 +89,7 @@ def mva_multiclass(routL, servrates, nClassL, queueType):
         for r in range(0,n_class):
             print "N",k,i+1,r+1, "=", lam[k][r],"*",T[k][i][r],"*",e[r][i], "=",N[k][i][r]
 
-    return  [lam1,lam2], T[nClassL]
+    return  [lam1, lam2], T[nClassL]
 
 
 def getVisitRatios(routL):
@@ -113,10 +115,8 @@ def updateRates(ratesL, adjustIndex, newVal):
 	newL = ratesL[:]
 	newL[adjustIndex[0]][adjustIndex[1]] = newVal
 	return newL
+
 		
-
-
-
 def adjustRate(routL, servrates, cusL, qTypeL, adjustIndex, maxError):
 	#print "In adjustRate:"
 	rateToAdjust = servrates[adjustIndex[0]][adjustIndex[1]]
@@ -149,7 +149,7 @@ def runmepan(msg):
     rout = [[0,1,0,0],[0,0,1,0],[0,0,0,1],[1,0,0,0]]
     for x in range(10,11):
         #print "service rate for the reader local computation: ",x
-        servrates = [[1.0,1.0],[1.0,1.0],[1.0,x],[1.0,0.7]]
+        servrates = np.array([[1.0,1.0],[1.0,1.0],[1.0,x],[1.0,0.7]])
         la,T = mva_multiclass([rout,rout], servrates, (2,2), [1,0,1,0])
         print "lambda before adjusting rates"
         print la[0],la[1]
@@ -159,3 +159,14 @@ def runmepan(msg):
         print newRates
         l,T = mva_multiclass([rout,rout], newRates, (2,2), [1,0,1,0])
         print l[0],l[1]
+
+def closedsingleclasstest():
+    p_back = 0.5
+    R = np.zeros((7,7))
+    R[1,2:5] = 1.0/3
+    R[2:5,5:7] = (1-p_back)/2.0
+    R[2:5,1] = p_back
+    R[5:7,2:5] = 1/3.0
+    S = np.array([0.2, 0.5, 0.8, 0.8, 0.8, 1.8, 1.8])
+    return mva_multiclass([R], S, (20,), [1,0,0,0,0,0,0])
+    #return S, R
